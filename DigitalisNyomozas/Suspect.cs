99afc1d377939d473b165ba487419f92f68b42b3
Bálint <gyarfas.balint@petrik.hu>
2026-01-29 14:27:20 +0100

    suspect class kesz

---
+++ DigitalisNyomozas/Suspect.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalisNyomozas
+{
+	internal class Suspect
+	{
+		private Person gyanusitott;
+		private int gyanusitottsagi_szint;
+		private string status;
+
+		public Suspect(Person gyanusitott, int gyanusitottsagi_szint, string status)
+		{
+			this.gyanusitott = gyanusitott;
+			this.gyanusitottsagi_szint = gyanusitottsagi_szint;
+			this.status = status;
+		}
+
+		public int Gyanusitottsagi_szint { get => gyanusitottsagi_szint; set => gyanusitottsagi_szint = value; }
+		public string Status { get => status; set => status = value; }
+		internal Person Gyanusitott { get => gyanusitott; set => gyanusitott = value; }
+	}
+}
