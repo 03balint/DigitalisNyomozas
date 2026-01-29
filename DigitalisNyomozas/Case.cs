60823e7efec1adcdee760fdef2febc23ed87a270
Bálint <gyarfas.balint@petrik.hu>
2026-01-29 14:24:04 +0100

    casestatus class kesz

--- DigitalisNyomozas/Case.cs
+++ DigitalisNyomozas/Case.cs
@@ -11,11 +11,11 @@ namespace DigitalisNyomozas
 		private string ugy_azonosito;
 		private string cim;
 		private string leiras;
-		private string allapot;
+		private CaseStatus allapot;
 		private List<string> szemelyek;
 		private List<string> bizonyitekok;
 
-		public Case(string ugy_azonosito, string cim, string leiras, string allapot, List<string> szemelyek, List<string> bizonyitekok)
+		public Case(string ugy_azonosito, string cim, string leiras, CaseStatus allapot, List<string> szemelyek, List<string> bizonyitekok)
 		{
 			this.ugy_azonosito = ugy_azonosito;
 			this.cim = cim;
@@ -28,8 +28,8 @@ namespace DigitalisNyomozas
 		public string Ugy_azonosito { get => ugy_azonosito; set => ugy_azonosito = value; }
 		public string Cim { get => cim; set => cim = value; }
 		public string Leiras { get => leiras; set => leiras = value; }
-		public string Allapot { get => allapot; set => allapot = value; }
 		public List<string> Szemelyek { get => szemelyek; set => szemelyek = value; }
 		public List<string> Bizonyitekok { get => bizonyitekok; set => bizonyitekok = value; }
+		internal CaseStatus Allapot { get => allapot; set => allapot = value; }
 	}
 }
---
+++ DigitalisNyomozas/CaseStatus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalisNyomozas
+{
+	internal class CaseStatus
+	{
+		private string status;
+
+		public CaseStatus(string status)
+		{
+			this.status = status;
+		}
+
+		public string Status { get => status; set => status = value; }
+	}
+}
