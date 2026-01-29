99fce58a36553a80228caa0f0277b563872d7b60
Bálint <gyarfas.balint@petrik.hu>
2026-01-29 14:31:36 +0100

    timelineevent class kesz

---
+++ DigitalisNyomozas/TimelineEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalisNyomozas
+{
+	internal class TimelineEvent
+	{
+		private DateTime datum;
+		private string esemeny_leirasa;
+
+		public TimelineEvent(DateTime datum, string esemeny_leirasa)
+		{
+			this.datum = datum;
+			this.esemeny_leirasa = esemeny_leirasa;
+		}
+
+		public DateTime Datum { get => datum; set => datum = value; }
+		public string Esemeny_leirasa { get => esemeny_leirasa; set => esemeny_leirasa = value; }
+	}
+}
